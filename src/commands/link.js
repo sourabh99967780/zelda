ea61228c03f1c9ca122ddb4a865bc206b12c59e7
Sourabh Kumar <sourabh99967780@gmail.com>
2021-03-18 00:49:20 +0530

    Added link rules

--- src/commands/link.js
+++ src/commands/link.js
@@ -1,7 +1,19 @@
-const neo4j = require('./plugins/neo4j');
-const makeQuery = require('./plugins/makeQuery');
+const neo4j = require('../plugins/neo4j');
+const makeQuery = require('../plugins/makeQuery');
 
 module.exports = async (msg, args) => {
 	makeQuery('Random Data');
+	const msgAuthor = msg.author;
+	const msgContent = msg.content.substr(6, msg.content.length);
+	if (!msgContent.includes('https' || 'http')) return;
+	console.log(args);
+	const node = {
+		'id': Math.random(10),
+		'user': msgAuthor.username,
+		'userId': msgAuthor.id,
+		'discriminator': msgAuthor.discriminator,
+		'message': msgContent,
+	};
+	console.log(node);
 	await msg.channel.send('Your links are being stored somewhere and I won\'t tell you where.');
 };
